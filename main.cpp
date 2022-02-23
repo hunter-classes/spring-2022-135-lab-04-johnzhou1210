5a5e25cd81212464ea64df7adc3ea81366a2b39f
johnzhou1210 <john.zhou81@myhunter.cuny.edu>
2022-02-22 20:22:38 -0500

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ std::string cross(int size);
 std::string lower(int size);
 std::string upper(int size);
 std::string trapezoid(int width, int height);
+std::string checkerboard3x3(int width, int height);
 
 int main()
 {
